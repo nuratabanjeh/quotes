92c024fe9640ccb749d712a3caa5ef06a8d0f8fe
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-04-04 16:59:19 +0300

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+alert('Welcome To the Quotes Library');
+var yourname = prompt('what is your name?');
+console.log(typeof yourname, yourname);
+
+var yourage = prompt('what is your age?');
+console.log(typeof yourage, yourage);
+
+ var city = prompt('in which city of jordan are you?');
+ console.log(typeof city , city);
+
+ var quotetype = prompt('what type of quotes you are searching for?' , 'inspiration, wisdom, success or music QUOTES')
+ console.log(typeof quotetype, quotetype);
+
+ alert( 'your name is ' + yourname + ' you are ' + yourage + 'years old' + ' also you are in jordan -' + city + ' and you are looking for ' + quotetype  );
